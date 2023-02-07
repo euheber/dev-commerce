5b9bde9174f0a163f34ed483f833bd4590b2a8fe
euheber <euheber1@gmail.com>
2023-02-07 18:55:10 -0300

    fixed: app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-    <p>teste</p>
-    <p>teste</p>
-    <p>teste</p>
+    <Header />
+    <RouterView></RouterView>
 </template>
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/header/header.vue'
 </script>
-
 
 <style scoped lang="scss" />
 

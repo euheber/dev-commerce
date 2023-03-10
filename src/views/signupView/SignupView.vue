5c46a4872fec2f4eddd9991d8ad91780f3255665
Clinton Rocha <clintongoncalvesrocha@gmail.com>
2023-03-10 19:37:03 -0300

    feat: input validation at registration added

--- src/views/signupView/SignupView.vue
+++ src/views/signupView/SignupView.vue
@@ -4,26 +4,33 @@
             <div class="illustration">
                 <BackgroundSignup />
             </div>
-            <form @submit.prevent>
+            <form @submit.prevent="validateForm">
 
                 <div class="card">
                     <a class="singup">Sign Up</a>
-                    <div class="inputBox1">
-                        <input type="email" required />
-                        <span class="user">Email</span>
-                    </div>
 
                     <div class="inputBox">
-                        <input type="text" required />
+                        <input type="text" v-model="name" />
                         <span>Username</span>
                     </div>
+                    <small v-if="nameError">{{ nameError }}</small>
+
+                    <div class="inputBox1">
+                        <input type="email" v-model="email" />
+                        <span class="user">Email</span>
+                    </div>
+                    <small v-if="emailError">{{ emailError }}</small>
 
                     <div class="inputBox">
-                        <input type="password" required />
+                        <input type="password" v-model="password" />
                         <span>Password</span>
-                    </div>
 
-                    <button class="enter" type="submit" @click="confirmAccess">Register</button>
+                    </div>
+                    <small v-if="passwordError">{{ passwordError }}</small>
+                    <p> The password must be at least 8 characters long, contain at least one uppercase letter, one
+                        lowercase letter, one number and one special character.</p>
+
+                    <button class="enter" type="submit">Register</button>
 
                 </div>
 
@@ -37,6 +44,7 @@ import { useUserStateStore } from '../../stores/UserStateStore';
 import BackgroundSignup from '../../assets/SVG/BackgroundSignup.vue'
 import router from '../../router';
 import { onMounted } from 'vue';
+import { ref } from 'vue';
 
 const userState = useUserStateStore();
 
@@ -46,12 +54,56 @@ onMounted(() => {
     }
 })
 
+const name = ref('');
+const email = ref('');
+const password = ref('');
+const nameError = ref('');
+const emailError = ref('');
+const passwordError = ref('');
 
 function confirmAccess() {
     localStorage.setItem("userSate", true);
     userState.LOGIN();
     router.push({ path: '/' })
 }
+const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+};
+const isValidPassword = (password) => {
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    return passwordRegex.test(password);
+};
+
+const validateForm = () => {
+    nameError.value = '';
+    emailError.value = '';
+    passwordError.value = '';
+
+    if (!name.value) {
+        nameError.value = 'Name is required';
+        return;
+    }
+
+    if (!email.value) {
+        emailError.value = 'E-mail is mandatory';
+        return;
+    } else if (!isValidEmail(email.value)) {
+        emailError.value = 'Invalid e-mail';
+        return;
+    }
+
+    if (!password.value) {
+        passwordError.value = 'Password is mandatory';
+        return;
+    } else if (!isValidPassword(password.value)) {
+        passwordError.value = 'Invalid password'
+        return;
+    }
+    confirmAccess();
+};
+
+
 </script>
 
 

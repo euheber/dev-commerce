045306623d1d1651713011ccca5dbd05728b0106
Clinton Rocha <clintongoncalvesrocha@gmail.com>
2023-03-11 07:03:10 -0300

    feat: validation input login

--- src/views/loginView/LoginView.vue
+++ src/views/loginView/LoginView.vue
@@ -6,20 +6,26 @@
         <BackgroundLogin />
       </div>
 
-      <form>
+      <form @submit.prevent="validateForm">
 
         <div class="card">
-          <a class="login">Log in</a>
-          <div class="inputBox">
-            <input type="email" required />
-            <span class="user">Email</span>
-          </div>
+          <h1 class="login">Log in</h1>
+
+
 
           <div class="inputBox">
-            <input type="password" required />
+            <input type="email" v-model="email" />
+            <span class="user">Email</span>
+          </div>
+          <small v-if="emailError">{{ emailError }}</small>
+
+          <div class="inputBox">
+            <input type="password" v-model="password" />
             <span>Password</span>
           </div>
-          <button class="enter" @click="confirmAccess">Enter</button>
+          <small v-if="passwordError">{{ passwordError }}</small>
+
+          <button class="enter" type="submit">Enter</button>
 
         </div>
 
@@ -31,7 +37,7 @@
 </template>
 <script setup>
 
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import BackgroundLogin from '../../assets/SVG/BackgroundLogin.vue';
 import router from '../../router';
 import { useUserStateStore } from '../../stores/UserStateStore';
@@ -51,6 +57,44 @@ function confirmAccess() {
   router.push({ path: '/' })
 }
 
+
+const email = ref('');
+const password = ref('');
+const emailError = ref('');
+const passwordError = ref('');
+
+
+const isValidEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+const isValidPassword = (password) => {
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+  return passwordRegex.test(password);
+};
+
+const validateForm = () => {
+
+  emailError.value = '';
+  passwordError.value = '';
+
+  if (!email.value) {
+    emailError.value = 'E-mail is mandatory';
+    return;
+  } else if (!isValidEmail(email.value)) {
+    emailError.value = 'Invalid e-mail';
+    return;
+  }
+
+  if (!password.value) {
+    passwordError.value = 'Password is mandatory';
+    return;
+  } else if (!isValidPassword(password.value)) {
+    passwordError.value = 'Invalid password'
+    return;
+  }
+  confirmAccess()
+};
 
 
 </script>

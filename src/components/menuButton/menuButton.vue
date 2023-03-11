5e10fcc2455c367840547a0e6e701b86f6468476
Clinton Rocha <clintongoncalvesrocha@gmail.com>
2023-03-11 19:49:43 -0300

    update: update component menuButton
    svg shoppingCart and UserIcon update

--- src/components/menuButton/menuButton.vue
+++ src/components/menuButton/menuButton.vue
@@ -4,15 +4,11 @@
         <div class="button" @click="LogoutAccount" title='Logout'>
             <Logout />
         </div>
-        <div class="button" title='Config'>
-            <ConfigIcon />
-        </div>
     </div>
 </template>
 <script setup>
 
 import Logout from '../../assets/SVG/Logout.vue'
-import ConfigIcon from '../../assets/SVG/ConfigIcon.vue'
 import UserIcon from '../../assets/SVG/UserIcon.vue'
 
 import { ref } from 'vue';

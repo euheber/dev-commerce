da891ccfe2686f4aa8506b98fb7a72f9f24f58cc
euheber <euheber1@gmail.com>
2023-03-12 18:09:36 -0300

    fix: empty shopping cart text

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -36,7 +36,7 @@
             <Transition enter-active-class="animate__animated animate__fadeIn"
                 leave-active-class="animate__animated animate__fadeOut" mode="out-in">
                 <span v-show="emptyCartShop">
-                    Your  cart shopping is empty
+                    Your shopping cart is empty
                 </span>
             </Transition>
         </div>
